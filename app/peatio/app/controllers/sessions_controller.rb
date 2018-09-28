203fad85701363ea19ecfca8dfe7f2f035b59edc
Ubuntu <ubuntu@ip-172-31-47-15.us-west-2.compute.internal>
2018-09-28 22:52:29 +0000

    fix

--- app/peatio/app/controllers/sessions_controller.rb
+++ app/peatio/app/controllers/sessions_controller.rb
@@ -6,9 +6,12 @@ class SessionsController < ApplicationController
   before_action :auth_anybody!, only: :failure
 
   def create
-    #@member = Member.from_auth(auth_hash)
+   # @member = Member.from_auth(auth_hash)
     @member = Member.first
-
+if @member.nil?
+	HH
+end
+    
     return redirect_on_unsuccessful_sign_in unless @member
     return redirect_to(root_path, alert: t('.disabled')) if @member.disabled?
 
